72328f52c579964f7e5fd7551fc68b9376878653
Javed <nadafjaved92@gmail.com>
2026-03-23 15:35:32 +0530

    repated _char

---
+++ Main (4).java
@@ -0,0 +1,31 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	String name = "programming";
+	
+	
+	for(int i = 0; i < name.length(); i++){
+	    char ch = name.charAt(i);
+	    if(name.indexOf(ch) !=i){
+	        continue;
+	    }
+	    boolean found = false;
+	    int count =0;
+	    
+	    for(int j = 0; j < name.length(); j++){
+	        if(ch == name.charAt(j)){
+	            found  = true;
+	            count++;
+	            
+	        }
+	    }
+	    if(count ==1){
+	        System.out.println(ch +"="+count);
+	    }
+// 	   else{
+// 	       System.out.println(ch+"="+count);
+// 	    }
+	}
+	}
+}
