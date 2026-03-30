e98c467ec2d272ea15856f0454820c765ed7c733
Javed <nadafjaved92@gmail.com>
2026-03-31 00:01:39 +0530

    using_inbuilt_functionSorting

---
+++ Main (7).java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = {4, 1, 6, 7};
+        
+        
+        Arrays.sort(arr);
+       
+       System.out.println(Arrays.toString(arr));
+    }
+}
