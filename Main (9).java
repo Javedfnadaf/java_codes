86bd365fa10e120beb2ac1627edcfcde40948738
Javed <nadafjaved92@gmail.com>
2026-04-03 15:32:31 +0530

    Inarray_adding_somevalue_after_string

---
+++ Main (9).java
@@ -0,0 +1,27 @@
+public class Main {
+	public static void main(String[] args) {
+   
+   
+   String name  = "abacsc";
+   
+   for(int i = 0; i < name.length(); i++){
+       
+       char ch = name.charAt(i);
+       
+       System.out.print(ch);
+       
+       
+       if(ch == 'a'){
+           System.out.print("1");
+           
+       }
+       
+   }
+   
+       
+       
+       
+   
+	
+	}
+}
