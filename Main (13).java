9594c4641d25adb2da59acd684ec732f304702d3
Javed <nadafjaved92@gmail.com>
2026-04-09 15:51:03 +0530

    reverse_char

---
+++ Main (13).java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+       
+     String s = "javed";
+
+char ch = s.charAt(0);
+System.out.println(ch);      
+            
+           
+        
+
+    }
+}
