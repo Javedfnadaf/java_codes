d4aee6a6ac5115bc18d70f9dbd8283afba3db600
Javed <nadafjaved92@gmail.com>
2026-04-18 23:59:19 +0530

    updated_linkedList

---
+++ LinkedList_new_updated.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        
+       LinkedList<String>list = new LinkedList<>();
+       
+       list.add("javed");
+       list.add("nisar");
+       list.add("harish");
+       
+       
+        System.out.println(list.set(1,"ali"));
+    //   list.addFirst("om");
+    //   list.addLast("arati");
+       
+    //   list.removeFirst();
+    //   list.removeLast();
+      
+    //   System.out.println(list.get(1));
+    //   System.out.println(list.getFirst());
+    //   System.out.println(list.getLast());
+       
+       
+    //   System.out.println(list.contains("javed"));
+    //   System.out.println(list.size());
+       
+       
+       System.out.println(list);
+       
+    }
+}
